9a547296a699ef9c4ab460d5274730e8f54f7651
34_Kydeeh <volkada1128@gmail.com>
2022-07-21 01:07:42 +0800

    no message

--- src/main/java/agjs/controller/SpringMvcController.java
+++ src/main/java/agjs/controller/SpringMvcController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package agjs.controller;
 
 import java.net.http.HttpRequest;
@@ -26,42 +25,6 @@ public class SpringMvcController {
 		System.out.println("SpingMvcController init");
 	}
 
-=======
-//package agjs.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import com.google.gson.Gson;
-//
-//import agjs.service.SpringMvcService;
-//
-//@Controller
-//@RequestMapping("/")
-////@WebServlet("/")
-//public class SpringMvcController {
-//
-//	@Autowired
-//	private SpringMvcService springMvcService;
-//	private Gson gson;
-//
-//	public SpringMvcController() {
-//
-//		System.out.println("SpingMvcController init");
-//	}
-//
->>>>>>> origin/dev_test
-//	@GetMapping("/**")
-//	public String ViewResolver() {
-//
-//		System.out.println("ViewResolver");
-//		
-//		return "index";
-//
-//	}
-<<<<<<< HEAD
 
 	@GetMapping("/select")
 	public String select() {
@@ -73,16 +36,3 @@ public class SpringMvcController {
 	}
 
 }
-=======
-//
-//	@GetMapping("/select")
-//	public String select() {
-//
-//		gson = new Gson();
-//		System.out.println(gson.toJson(springMvcService.getRoomUsedRecordAll()));
-//
-//		return "index";
-//	}
-//
-//}
->>>>>>> origin/dev_test
