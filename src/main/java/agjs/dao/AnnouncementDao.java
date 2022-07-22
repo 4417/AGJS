ad1a47f67857434f783c158b6130b26b765d6f8a
Jing <pchinglitw@gmail.com>
2022-07-22 19:20:18 +0800

    feat：(公告) Dao 建立

--- src/main/java/agjs/dao/AnnouncementDao.java
+++ src/main/java/agjs/dao/AnnouncementDao.java
@@ -5,11 +5,11 @@ import java.util.List;
 import agjs.bean.AnnouncementPo;
 
 public interface AnnouncementDao {
-	List<AnnouncementPo> selectKeyword();
-	List<AnnouncementPo> selectStartDate();
-	List<AnnouncementPo> selectEndDate();
-	List<AnnouncementPo> selectType();
-	List<AnnouncementPo> insertAnm();
-	List<AnnouncementPo> updateAnm();
-	List<AnnouncementPo> deleteAnm();
+	List<AnnouncementPo> selectKeyword(String keyword);
+	List<AnnouncementPo> selectStartDate(AnnouncementPo announcementPo);
+	List<AnnouncementPo> selectEndDate(AnnouncementPo announcementPo);
+	List<AnnouncementPo> selectType(AnnouncementPo announcementPo);
+	AnnouncementPo insertAnm(AnnouncementPo announcementPo);
+	AnnouncementPo updateAnm(Integer anmId);
+	List<AnnouncementPo> deleteAnm(Integer anmId);
 }
