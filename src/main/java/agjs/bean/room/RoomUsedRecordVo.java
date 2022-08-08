e2018c5c7dccb5a2a4c7a48d8c7605e806bd2b82
02_Sumi <upash123@gmail.com>
2022-08-08 13:42:52 +0800

    feat:新增複合主鍵

---
+++ src/main/java/agjs/bean/room/RoomUsedRecordVo.java
@@ -0,0 +1,60 @@
+package agjs.bean.room;
+
+import java.util.Date;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class RoomUsedRecordVo {
+	@EmbeddedId
+	private RoomUsedRecordIdVo roomUsedRecordIdVo;
+	private Integer roomStyleId;
+	private String roomName;
+	@JsonFormat(pattern = "yyyy-mm-dd", timezone = "GMT+8")
+	private Date orderEndDate;
+	private String userName;
+	private Integer source;
+
+	public Integer getRoomStyleId() {
+		return roomStyleId;
+	}
+
+	public void setRoomStyleId(Integer roomStyleId) {
+		this.roomStyleId = roomStyleId;
+	}
+
+	public String getRoomName() {
+		return roomName;
+	}
+
+	public void setRoomName(String roomName) {
+		this.roomName = roomName;
+	}
+
+	public Date getOrderEndDate() {
+		return orderEndDate;
+	}
+
+	public void setOrderEndDate(Date orderEndDate) {
+		this.orderEndDate = orderEndDate;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Integer getSource() {
+		return source;
+	}
+
+	public void setSource(Integer source) {
+		this.source = source;
+	}
+}
